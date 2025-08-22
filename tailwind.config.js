4107bdba24ba03d41bbdb8dd887ec23256e5b645
gfdezz <gorkafdez0@gmail.com>
2025-08-23 01:33:50 +0200

    fix lololo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "./assets/**/*.js"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary:  "#8b5cf6", // violeta
+        secondary:"#ec4899", // rosa
+      },
+    },
+  },
   plugins: [],
-}
+};
